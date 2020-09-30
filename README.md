253010108e61f5488925556f0cb4fa4c1683f65c
user <watchosastrom3104@gmail.com>
2020-09-30 11:11:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TensorFlow CNN Collections for HASC
+This is the repository of CNN models implemented for human activity recognition (HASC), like `tensorflow.keras.applications`.
+
+The models implemented here can also be used as a source domain for sensor-based task (e.g. sidewalk surface type estimation).
+
+## Modules
+
+- [`vgg16`](reference/reference.md#applicationsvgg16vgg16) module : VGG16 model for HASC for Keras.
+- `vgg19` module : VGG19 model for HASC for Keras, **Not implemented yet**.
+- `resnet` module : ResNet model for HASC for Keras, **Not implemented yet**.
+- `alexnet` module : AlexNet model for HASC for Keras, **Not implemented yet**.
---
+++ applications/vgg16.py
@@ -0,0 +1,153 @@
+from tensorflow.keras.layers import Input, Flatten, Dense
+from tensorflow.keras.layers import Conv1D, MaxPooling1D, Dropout
+from tensorflow.keras.layers import GlobalAveragePooling1D, GlobalMaxPooling1D
+from tensorflow.keras.models import Model
+
+from applications.base import DLModelBuilder
+
+import os
+
+
+# ConvBlock
+class ConvBlock:
+    def __init__(self, repeat, filters, kernel_size=3, strides=1, padding='same', activation='relu',
+                 kernel_initializer='he_normal', pool_size=2):
+        """
+        ConvBlock for VGG
+            repeat: the number of Conv1D
+            filters: the number of filter of Conv1D
+            kernel_size: the kernel_size of Conv1D, default `3`
+            strides: the strides of Conv1D, default `1`
+            padding: the padding of Conv1D and MaxPooling1D, default `'same'`
+            activation: the activation function of Conv1D, default `'relu'`
+            kernel_initializer: the kernel_initializer of Conv1D, default `'he_normal'`
+            pool_size: the pool_size of MaxPooling1D, default `2`
+        """
+        self.repeat = repeat
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.padding = padding
+        self.activation = activation
+        self.kernel_initializer = kernel_initializer
+        self.pool_size = pool_size
+
+    def __call__(self, x):
+        for _ in range(0, self.repeat):
+            x = Conv1D(self.filters, kernel_size=self.kernel_size, strides=self.strides, padding=self.padding,
+                       activation=self.activation, kernel_initializer=self.kernel_initializer)(x)
+
+        x = MaxPooling1D(pool_size=self.pool_size, padding=self.padding)(x)
+        return x
+
+
+# he_normalで初期化するVGG16
+class BaseVGG16(DLModelBuilder):
+    def __init__(self, kernel_size=3, strides=1, kernel_initializer='he_normal',
+                 padding='same', input_shape=(256 * 3, 1), num_classes=6, classifier_activation='softmax'):
+        """
+        VGG16
+            kernel_size: kernel_size of Conv1D, default `3`
+            strides: strides of Conv1D, default `1`
+            kernel_initializer: kernel_initializer of Conv1D and Dense (Fully-connected layers), default `'he_normal'`
+            padding: padding of Conv1D and MaxPooling1D, default `'same'`
+            input_shape: shape of Input, default `(768, 1)`
+            num_classes: The number of target classes
+            classifier_activation: The activation function to use on the "top" layer, default `"softmax"`
+        """
+        super(BaseVGG16, self).__init__(kernel_size=kernel_size, strides=strides, kernel_initializer=kernel_initializer,
+                                        padding=padding, input_shape=input_shape, num_classes=num_classes)
+        self.classifier_activation = classifier_activation
+        self.model_name = "VGG16"
+
+    def __call__(self, *args, **kwargs):
+        model = self.get_model()
+        return model
+
+    def get_model(self):
+        inputs = Input(shape=self.input_shape)
+        x = ConvBlock(2, 16, kernel_size=self.kernel_size, strides=self.strides, padding=self.padding,
+                      kernel_initializer=self.kernel_initializer)(inputs)
+        x = ConvBlock(2, 32, kernel_size=self.kernel_size, strides=self.strides, padding=self.padding,
+                      kernel_initializer=self.kernel_initializer)(x)
+        x = ConvBlock(3, 64, kernel_size=self.kernel_size, strides=self.strides, padding=self.padding,
+                      kernel_initializer=self.kernel_initializer)(x)
+        x = ConvBlock(3, 128, kernel_size=self.kernel_size, strides=self.strides, padding=self.padding,
+                      kernel_initializer=self.kernel_initializer)(x)
+        x = ConvBlock(3, 128, kernel_size=self.kernel_size, strides=self.strides, padding=self.padding,
+                      kernel_initializer=self.kernel_initializer)(x)
+
+        x = Flatten()(x)
+        x = Dense(1024, activation='relu', kernel_initializer=self.kernel_initializer)(x)
+        x = Dropout(0.5)(x)
+        x = Dense(1024, activation='relu', kernel_initializer=self.kernel_initializer)(x)
+        x = Dropout(0.5)(x)
+        y = Dense(self.num_classes, activation=self.classifier_activation)(x)
+
+        model = Model(inputs=inputs, outputs=y)
+        return model
+
+
+# VGG16を読み込む関数
+def VGG16(include_top=True, weights='hasc', input_shape=None, pooling=None, classes=6, classifier_activation='softmax'):
+    """
+    applications.vgg16.VGG16
+        Arguments
+            include_top : whether to include the 3 fully-connected layers at the top of the network.
+            weights : one of 'None' (he_normal initialization), 'hasc' (pre-training on HASC), or the path to the weights file to be loaded.
+            input_shape : optional shape tuple, default `(768, 1)` (with channels_last data format).
+            pooling : optional pooling mode for feature extraction when `include_top` is False.
+                        `None` means that the output of the model will be applied to the 3D tensor output of the last convolutional block.
+                        `avg` means that global average pooling will be applied to the output of the last convolutioinal block, and thus the output od the model will be a 2D tensor.
+                        `max` means that global max pooling will be applied.
+            classes : optional number of classes to classify images into, only to be specified if `include_top` is True, and if no weights argument is specified, default 6.
+            classifier_activation : A `str` or callable. The activation function to use on the "top" layer. Ignored unless `include_top=True`. Set classifier_activation=None to return the logits of the "top" layer, default `softmax`.
+        Returns
+            A `tensorflow.keras.Model` instance.
+    """
+
+    if input_shape is None:
+        input_shape = (256 * 3, 1)
+
+    vgg16 = BaseVGG16(input_shape=input_shape, num_classes=classes, classifier_activation=classifier_activation)
+    model = vgg16()
+
+    # topを含むとき
+    if include_top:
+        if weights is not None:
+            # hascで初期化
+            if weights in ['hasc', "HASC"]:
+                weights = 'weights/vgg16/vgg16_hasc_weights_{}.hdf5'.format(input_shape[0])
+
+            # hasc or weights fileで初期化
+            if os.path.exists(weights):
+                model.load_weights(weights)
+            else:
+                # 重みのファイルがなかったらhe_normal初期化のまま返す
+                print("Not exist weights: {}".format(weights))
+
+    # topを含まないとき
+    else:
+        if weights is not None:
+            # hascで初期化
+            if weights in ['hasc', "HASC"]:
+                weights = 'weights/vgg16/vgg16_hasc_weights_{}.hdf5'.format(input_shape[0])
+
+            # hasc or weights fileで初期化
+            if os.path.exists(weights):
+                model.load_weights(weights)
+            else:
+                # 重みのファイルがなかったらhe_normal初期化のまま返す
+                print("Not exist weights: {}".format(weights))
+
+        if pooling is None:
+            # topを削除する
+            model = Model(inputs=model.input, outputs=model.layers[-7].output)
+        elif pooling == 'avg':
+            y = GlobalAveragePooling1D()(model.layers[-7].output)
+            model = Model(inputs=model.input, outputs=y)
+        elif pooling == 'max':
+            y = GlobalMaxPooling1D()(model.layers[-7].output)
+            model = Model(inputs=model.input, outputs=y)
+
+    return model
