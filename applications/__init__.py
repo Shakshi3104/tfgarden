8e7cb2dcef5f9be1d2e4b4f6aeb0db356e35fe48
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-05-30 09:24:41 +0900

    Add __init__.py

---
+++ applications/__init__.py
@@ -0,0 +1,32 @@
+from .densenet import *
+from .efficientnet import *
+from .efficientnet_lite import *
+from .inception_resnet_v2 import *
+from .mnasnet import *
+from .mobilenet import *
+from .mobilenet_v2 import *
+from .mobilenet_v3 import *
+from .nasnet import *
+from .pyramidnet import *
+from .resnet import *
+from .vgg11 import *
+from .vgg13 import *
+from .vgg16 import *
+from .vgg19 import *
+
+
+__all__ = [
+    "DenseNet121", "DenseNet169", "DenseNet201",
+    "EfficientNetB0", "EfficientNetB1", "EfficientNetB2", "EfficientNetB3", "EfficientNetB4", "EfficientNetB5",
+    "EfficientNetB6", "EfficientNetB7",
+    "EfficientNet_lite0", "EfficientNet_lite1", "EfficientNet_lite2", "EfficientNet_lite3", "EfficientNet_lite4",
+    "InceptionResNetV2",
+    "MnasNet",
+    "MobileNet",
+    "MobileNetV2",
+    "MobileNetV3Large", "MobileNetV3Small",
+    "NASNetLarge", "NASNetMobile",
+    "PyramidNet18", "PyramidNet34", "PyramidNet50", "PyramidNet101", "PyramidNet152",
+    "ResNet16", "ResNet18", "ResNet34",
+    "VGG11", "VGG13", "VGG16", "VGG19",
+]
