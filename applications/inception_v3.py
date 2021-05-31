4dc69de39936f6c2a4cf10e6a99f59ba2b664d9b
Shakshi3104 <watchosastrom3104@gmail.com>
2021-05-31 14:24:36 +0900

    bug fix

--- applications/inception_v3.py
+++ applications/inception_v3.py
@@ -212,7 +212,7 @@ def InceptionV3(include_top=True, weights='hasc', input_shape=None, pooling=None
 
     if weights is not None:
         if weights in ['hasc', "HASC"]:
-            weights = 'weights/mobilenet/mobilenet_hasc_weights_{}.hdf5'.format(int(input_shape[0] / 3))
+            weights = 'weights/inceptionv3/inceptionv3_hasc_weights_{}.hdf5'.format(int(input_shape[0] / 3))
 
         # hasc or weights fileで初期化
         if os.path.exists(weights):
