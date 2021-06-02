49bc87b618a9bacd945729e8567a7755d1d8889b
Shakshi3104 <watchosastrom3104@gmail.com>
2021-06-02 13:39:40 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="tfgarden",
+    version="3.0.0",
+    author="Shakshi3104",
+    description="TensorFlow model Garden for HASC",
+    url="https://github.com/Shakshi3104/tfgarden",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    python_requires=">=3.6, <4",
+    install_requires=["tensorflow"]
+)
