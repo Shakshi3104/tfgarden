3ae9fad0159e2010529ec05c66b8c48aabd80119
Shakshi3104 <watchosastrom3104@gmail.com>
2021-08-04 15:46:09 +0900

    Deprecated DLModelBuilder: MobileNet V3

--- src/tfgarden/applications/mobilenet_v3.py
+++ src/tfgarden/applications/mobilenet_v3.py
@@ -3,8 +3,6 @@ import os
 from tensorflow.keras import layers
 from tensorflow.keras.models import Model
 
-from .base import DLModelBuilder
-
 
 def _depth(v, divisor=8, min_value=None):
     if min_value is None:
@@ -128,91 +126,84 @@ class InvertedResBlock:
         return x
 
 
-class BaseMobileNetV3(DLModelBuilder):
-    def __init__(self, stack_fn, last_point_ch, input_shape=(256 * 3, 1), alpha=1.0, model_type='large',
-                 minimalistic=False, num_classes=6, dropout_rate=0.2, classifier_activation='softmax'):
-        self.stack_fn = stack_fn
-        self.last_point_ch = last_point_ch
-        self.input_shape = input_shape
-        self.alpha = alpha
-        self.model_type = model_type
-        self.minimalistic = minimalistic
-        self.num_classes = num_classes
-        self.dropout_rate = dropout_rate
-        self.classifier_activation = classifier_activation
+def MobileNetV3(stack_fn, last_point_ch, input_shape=(256 * 3, 1), alpha=1.0, minimalistic=False,
+                classes=6, dropout_rate=0.2, classifier_activation='softmax'):
+    inputs = layers.Input(shape=input_shape)
 
-    def __call__(self, *args, **kwargs):
-        model = self.get_model()
-        return model
+    if minimalistic:
+        kernel = 3
+        activation = relu
+        se_ratio = None
+    else:
+        kernel = 5
+        activation = hard_swish
+        se_ratio = 0.25
 
-    def get_model(self):
-        inputs = layers.Input(shape=self.input_shape)
+    x = inputs
+    x = layers.Conv1D(
+        16,
+        kernel_size=3,
+        strides=2,
+        padding='same',
+        use_bias=False,
+        name='Conv'
+    )(x)
+    x = layers.BatchNormalization(
+        epsilon=1e-3, momentum=0.999, name='Conv/BatchNorm'
+    )(x)
+    x = activation(x)
 
-        if self.minimalistic:
-            kernel = 3
-            activation = relu
-            se_ratio = None
-        else:
-            kernel = 5
-            activation = hard_swish
-            se_ratio = 0.25
+    x = stack_fn(x, kernel, activation, se_ratio)
 
-        x = inputs
-        x = layers.Conv1D(
-            16,
-            kernel_size=3,
-            strides=2,
-            padding='same',
-            use_bias=False,
-            name='Conv'
-        )(x)
-        x = layers.BatchNormalization(
-            epsilon=1e-3, momentum=0.999, name='Conv/BatchNorm'
-        )(x)
-        x = activation(x)
+    last_conv_ch = _depth(x.shape[-1] * 6)
 
-        x = self.stack_fn(x, kernel, activation, se_ratio)
+    # if the width multiplier is grather than 1, we increase the number of output channels
+    if alpha > 1.0:
+        last_conv_ch = _depth(last_conv_ch * alpha)
+    x = layers.Conv1D(
+        last_conv_ch,
+        kernel_size=1,
+        padding='same',
+        use_bias=False,
+        name='Conv_1'
+    )(x)
+    x = layers.BatchNormalization(
+        epsilon=1e-3, momentum=0.999, name='Conv_1/BatchNorm'
+    )(x)
+    x = activation(x)
+    x = layers.Conv1D(
+        last_point_ch,
+        kernel_size=1,
+        padding='same',
+        use_bias=True,
+        name='Conv_2'
+    )(x)
+    x = activation(x)
 
-        last_conv_ch = _depth(x.shape[-1] * 6)
+    # top
+    x = layers.GlobalAveragePooling1D()(x)
+    x = layers.Reshape((1, last_point_ch))(x)
+    if dropout_rate > 0:
+        x = layers.Dropout(dropout_rate)(x)
+    x = layers.Conv1D(classes, kernel_size=1, padding='same', name='Logits')(x)
+    x = layers.Flatten()(x)
+    y = layers.Activation(activation=classifier_activation, name='Predictions')(x)
 
-        # if the width multiplier is grather than 1, we increase the number of output channels
-        if self.alpha > 1.0:
-            last_conv_ch = _depth(last_conv_ch * self.alpha)
-        x = layers.Conv1D(
-            last_conv_ch,
-            kernel_size=1,
-            padding='same',
-            use_bias=False,
-            name='Conv_1'
-        )(x)
-        x = layers.BatchNormalization(
-            epsilon=1e-3, momentum=0.999, name='Conv_1/BatchNorm'
-        )(x)
-        x = activation(x)
-        x = layers.Conv1D(
-            self.last_point_ch,
-            kernel_size=1,
-            padding='same',
-            use_bias=True,
-            name='Conv_2'
-        )(x)
-        x = activation(x)
-
-        # top
-        x = layers.GlobalAveragePooling1D()(x)
-        x = layers.Reshape((1, self.last_point_ch))(x)
-        if self.dropout_rate > 0:
-            x = layers.Dropout(self.dropout_rate)(x)
-        x = layers.Conv1D(self.num_classes, kernel_size=1, padding='same', name='Logits')(x)
-        x = layers.Flatten()(x)
-        y = layers.Activation(activation=self.classifier_activation, name='Predictions')(x)
-
-        model = Model(inputs, y)
-        return model
+    model = Model(inputs, y)
+    return model
 
 
-def __MobileNetV3Small(input_shape=None, alpha=1.0, minimalistic=False, classes=6, dropout_rate=0.2,
-                       classifier_activation='softmax'):
+def MobileNetV3Small(include_top=True, weights='hasc', input_shape=None, pooling=None, classes=6,
+                     classifier_activation='softmax',
+                     alpha=1.0, minimalistic=False):
+    if input_shape is None:
+        input_shape = (256 * 3, 1)
+
+    if weights in ['hasc', 'HASC'] and include_top and classes != 6:
+        raise ValueError('If using `weights` as `"hasc"` with `include_top`'
+                         ' as true, `classes` should be 6')
+
+
     def stack_fn(x, kernel, activation, se_ratio):
         def depth(d):
             return _depth(d * alpha)
@@ -230,57 +221,12 @@ def __MobileNetV3Small(input_shape=None, alpha=1.0, minimalistic=False, classes=
         x = InvertedResBlock(6, depth(96), kernel, 1, se_ratio, activation, 10)(x)
         return x
 
-    model = BaseMobileNetV3(stack_fn, 1024, input_shape, alpha, 'small', minimalistic,
-                            classes, dropout_rate, classifier_activation)()
-    return model
-
-
-def __MobileNetV3Large(input_shape=None, alpha=1.0, minimalistic=False, classes=6, dropout_rate=0.2,
-                       classifier_activation='softmax'):
-    def stack_fn(x, kernel, activation, se_ratio):
-        def depth(d):
-            return _depth(d * alpha)
-
-        x = InvertedResBlock(1, depth(16), 3, 1, None, relu, 0)(x)
-        x = InvertedResBlock(4, depth(24), 3, 2, None, relu, 1)(x)
-        x = InvertedResBlock(3, depth(24), 3, 1, None, relu, 2)(x)
-        x = InvertedResBlock(3, depth(40), kernel, 2, se_ratio, relu, 3)(x)
-        x = InvertedResBlock(3, depth(40), kernel, 1, se_ratio, relu, 4)(x)
-        x = InvertedResBlock(3, depth(40), kernel, 1, se_ratio, relu, 5)(x)
-        x = InvertedResBlock(6, depth(80), 3, 2, None, activation, 6)(x)
-        x = InvertedResBlock(2.5, depth(80), 3, 1, None, activation, 7)(x)
-        x = InvertedResBlock(2.3, depth(80), 3, 1, None, activation, 8)(x)
-        x = InvertedResBlock(2.3, depth(80), 3, 1, None, activation, 9)(x)
-        x = InvertedResBlock(6, depth(112), 3, 1, se_ratio, activation, 10)(x)
-        x = InvertedResBlock(6, depth(112), 3, 1, se_ratio, activation, 11)(x)
-        x = InvertedResBlock(6, depth(160), kernel, 2, se_ratio, activation, 12)(x)
-        x = InvertedResBlock(6, depth(160), kernel, 1, se_ratio, activation, 13)(x)
-        x = InvertedResBlock(6, depth(160), kernel, 1, se_ratio, activation, 14)(x)
-        return x
-
-    model = BaseMobileNetV3(stack_fn, 1280, input_shape, alpha, 'large', minimalistic,
-                            classes, dropout_rate, classifier_activation)()
-    return model
-
-
-def __MobileNetV3(type, include_top=True, weights='hasc', input_shape=None, pooling=None, classes=6,
-                  classifier_activation='softmax',
-                  alpha=1.0, minimalistic=False):
-    if input_shape is None:
-        input_shape = (256 * 3, 1)
-
-    if weights in ['hasc', 'HASC'] and include_top and classes != 6:
-        raise ValueError('If using `weights` as `"hasc"` with `include_top`'
-                         ' as true, `classes` should be 6')
-
-    if type == 'small':
-        model = __MobileNetV3Small(input_shape, alpha, minimalistic, classes, 0.2, classifier_activation)
-    elif type == 'large':
-        model = __MobileNetV3Large(input_shape, alpha, minimalistic, classes, 0.2, classifier_activation)
+    model = MobileNetV3(stack_fn, 1024, minimalistic=minimalistic, input_shape=input_shape,
+                        classes=classes, classifier_activation=classifier_activation)
 
     if weights is not None:
         if weights in ['hasc', "HASC"]:
-            weights = 'weights/mobilenetv3{}/mobilenetv3{}_hasc_weights_{}_{}.hdf5'.format(type, type,
+            weights = 'weights/mobilenetv3small/mobilenetv3small_hasc_weights_{}_{}.hdf5'.format(
                                                                                            int(input_shape[0]),
                                                                                            int(input_shape[1]))
 
@@ -309,19 +255,69 @@ def __MobileNetV3(type, include_top=True, weights='hasc', input_shape=None, pool
     return model
 
 
-def MobileNetV3Small(include_top=True, weights='hasc', input_shape=None, pooling=None, classes=6,
-                     classifier_activation='softmax',
-                     alpha=1.0, minimalistic=False):
-    model = __MobileNetV3('small', include_top, weights, input_shape, pooling, classes, classifier_activation, alpha,
-                          minimalistic)
-    return model
-
-
 def MobileNetV3Large(include_top=True, weights='hasc', input_shape=None, pooling=None, classes=6,
                      classifier_activation='softmax',
                      alpha=1.0, minimalistic=False):
-    model = __MobileNetV3('large', include_top, weights, input_shape, pooling, classes, classifier_activation, alpha,
-                          minimalistic)
+
+    if input_shape is None:
+        input_shape = (256 * 3, 1)
+
+    if weights in ['hasc', 'HASC'] and include_top and classes != 6:
+        raise ValueError('If using `weights` as `"hasc"` with `include_top`'
+                         ' as true, `classes` should be 6')
+
+    def stack_fn(x, kernel, activation, se_ratio):
+        def depth(d):
+            return _depth(d * alpha)
+
+        x = InvertedResBlock(1, depth(16), 3, 1, None, relu, 0)(x)
+        x = InvertedResBlock(4, depth(24), 3, 2, None, relu, 1)(x)
+        x = InvertedResBlock(3, depth(24), 3, 1, None, relu, 2)(x)
+        x = InvertedResBlock(3, depth(40), kernel, 2, se_ratio, relu, 3)(x)
+        x = InvertedResBlock(3, depth(40), kernel, 1, se_ratio, relu, 4)(x)
+        x = InvertedResBlock(3, depth(40), kernel, 1, se_ratio, relu, 5)(x)
+        x = InvertedResBlock(6, depth(80), 3, 2, None, activation, 6)(x)
+        x = InvertedResBlock(2.5, depth(80), 3, 1, None, activation, 7)(x)
+        x = InvertedResBlock(2.3, depth(80), 3, 1, None, activation, 8)(x)
+        x = InvertedResBlock(2.3, depth(80), 3, 1, None, activation, 9)(x)
+        x = InvertedResBlock(6, depth(112), 3, 1, se_ratio, activation, 10)(x)
+        x = InvertedResBlock(6, depth(112), 3, 1, se_ratio, activation, 11)(x)
+        x = InvertedResBlock(6, depth(160), kernel, 2, se_ratio, activation, 12)(x)
+        x = InvertedResBlock(6, depth(160), kernel, 1, se_ratio, activation, 13)(x)
+        x = InvertedResBlock(6, depth(160), kernel, 1, se_ratio, activation, 14)(x)
+        return x
+
+    model = MobileNetV3(stack_fn, 1280, minimalistic=minimalistic, input_shape=input_shape,
+                        classes=classes, classifier_activation=classifier_activation)
+
+    if weights is not None:
+        if weights in ['hasc', "HASC"]:
+            weights = 'weights/mobilenetv3small/mobilenetv3small_hasc_weights_{}_{}.hdf5'.format(
+                int(input_shape[0]),
+                int(input_shape[1]))
+
+        # hasc or weights fileで初期化
+        if os.path.exists(weights):
+            print("Load weights from {}".format(weights))
+            model.load_weights(weights)
+        else:
+            print("Not exist weights: {}".format(weights))
+
+    # topを含まないとき
+    if not include_top:
+        if pooling is None:
+            # topを削除する
+            model = Model(inputs=model.input, outputs=model.layers[-7].output)
+        elif pooling == 'avg':
+            y = layers.GlobalAveragePooling1D()(model.layers[-7].output)
+            model = Model(inputs=model.input, outputs=y)
+        elif pooling == 'max':
+            y = layers.GlobalMaxPooling1D()(model.layers[-7].output)
+            model = Model(inputs=model.input, outputs=y)
+        else:
+            print("Not exist pooling option: {}".format(pooling))
+            model = Model(inputs=model.input, outputs=model.layers[-7].output)
+
     return model
 
 
