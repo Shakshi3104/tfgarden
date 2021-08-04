6073ebc31ccac39e3721cd0772d4da086f9af3c3
Shakshi3104 <watchosastrom3104@gmail.com>
2021-08-04 14:36:00 +0900

    Deprecated DLModelBuilder: VGG16

--- src/tfgarden/applications/vgg16.py
+++ src/tfgarden/applications/vgg16.py
@@ -1,8 +1,11 @@
-from tensorflow.keras.layers import Input, Flatten, Dense, Dropout
+import os
+
+from tensorflow.keras.layers import Input, Flatten, Dense, GlobalAveragePooling1D, GlobalMaxPooling1D
 from tensorflow.keras.models import Model
 
-from .base import DLModelBuilder, ConvBlock
-from .vgg import VGG
+from .base import DLModelBuilder
+from src.tfgarden.applications import ConvBlock
+from .vgg import ConvBlock
 
 
 # he_normalで初期化するVGG16
@@ -70,10 +73,56 @@ def VGG16(include_top=True, weights='hasc', input_shape=None, pooling=None, clas
             A `tensorflow.keras.Model` instance.
     """
 
-    model = VGG(16, include_top=include_top, weights=weights, input_shape=input_shape, pooling=pooling, classes=classes,
-                classifier_activation=classifier_activation)
+    if input_shape is None:
+        input_shape = (256 * 3, 1)
 
-    return model
+    if weights in ['hasc', 'HASC'] and include_top and classes != 6:
+        raise ValueError('If using `weights` as `"hasc"` with `include_top`'
+                         ' as true, `classes` should be 6')
+
+    inputs = Input(shape=input_shape)
+    x = ConvBlock(2, 64)(inputs)
+    x = ConvBlock(2, 128)(x)
+    x = ConvBlock(3, 256)(x)
+    x = ConvBlock(3, 512)(x)
+    x = ConvBlock(3, 512)(x)
+
+    if include_top:
+        x = Flatten()(x)
+        x = Dense(4096, activation="relu", kernel_initializer="he_normal",
+                  name="fc1")(x)
+        x = Dense(4096, activation="relu", kernel_initializer="he_normal",
+                  name="fc2")(x)
+        y = Dense(classes, activation=classifier_activation, name="prediction")(x)
+
+        model_ = Model(inputs=inputs, outputs=y)
+
+        if weights is not None:
+            if weights in ['hasc', 'HASC']:
+                weights = 'weights/vgg16/vgg16_hasc_weights_{}_{}.hdf5'.format(input_shape[0], input_shape[1])
+
+            if os.path.exists(weights):
+                print("Load weights from {}".format(weights))
+                model_.load_weights(weights)
+            else:
+                print("Not exist weights: {}".format(weights))
+        return model_
+    else:
+        if pooling is None:
+            model_ = Model(inputs=inputs, outputs=x)
+            return model_
+        elif pooling == 'avg':
+            x = GlobalAveragePooling1D(name="avgpool")(x)
+            model_ = Model(inputs=inputs, outputs=x)
+            return model_
+        elif pooling == 'max':
+            x = GlobalMaxPooling1D(name="maxpool")(x)
+            model_ = Model(inputs=inputs, outputs=x)
+            return model_
+        else:
+            print("Not exist pooling option: {}".format(pooling))
+            model_ = Model(inputs=inputs, outputs=x)
+            return model_
 
 
 if __name__ == '__main__':
